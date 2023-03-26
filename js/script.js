def691c3c711ff54108bfadfa43e6b2d830f2e13
DAVID MARQUES <davidmarqqs@gmail.com>
2023-03-26 03:53:21 -0300

    feat: implementando os endpoints

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 
-const pokemonName = document.querySelector('.pokemon__name');
-const pokemonNumber = document.querySelector('.pokemon__number');
-const pokemonImage = document.querySelector('.pokemon__image');
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
 
 const form = document.querySelector('.form');
-const input = document.querySelector('.input__search');
+const input = document.querySelector('.input_search');
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
 let searchPokemon = 1;
 
-const fetchPokemon = async (pokemon) => {
+const buscarPokemon = async (pokemon) => {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
   if (APIResponse.status === 200) {
@@ -19,12 +19,12 @@ const fetchPokemon = async (pokemon) => {
   }
 }
 
-const renderPokemon = async (pokemon) => {
+const exibirPokemon = async (pokemon) => {
 
-  pokemonName.innerHTML = 'Loading...';
+  pokemonName.innerHTML = 'Carregando...';
   pokemonNumber.innerHTML = '';
 
-  const data = await fetchPokemon(pokemon);
+  const data = await buscarPokemon(pokemon);
 
   if (data) {
     pokemonImage.style.display = 'block';
@@ -35,26 +35,26 @@ const renderPokemon = async (pokemon) => {
     searchPokemon = data.id;
   } else {
     pokemonImage.style.display = 'none';
-    pokemonName.innerHTML = 'Not found :c';
+    pokemonName.innerHTML = 'Não localizado';
     pokemonNumber.innerHTML = '';
   }
 }
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  renderPokemon(input.value.toLowerCase());
+  exibirPokemon(input.value.toLowerCase());
 });
 
 buttonPrev.addEventListener('click', () => {
   if (searchPokemon > 1) {
     searchPokemon -= 1;
-    renderPokemon(searchPokemon);
+    exibirPokemon(searchPokemon);
   }
 });
 
 buttonNext.addEventListener('click', () => {
   searchPokemon += 1;
-  renderPokemon(searchPokemon);
+  exibirPokemon(searchPokemon);
 });
 
-renderPokemon(searchPokemon);
+exibirPokemon(searchPokemon);
